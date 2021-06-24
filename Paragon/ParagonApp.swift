40ef01b073478f62991077c86a2556fc8a6a5506
Fabrice Beya <beyafabrice@gmail.com>
2021-06-24 06:10:28 +0200

    Initial Commit

---
+++ Paragon/ParagonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParagonApp.swift
+//  Paragon
+//
+//  Created by Fabrice Beya on 2021/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ParagonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
